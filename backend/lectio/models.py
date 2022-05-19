c2617e0872c329bfee2a2c5eba131420b998c5a4
clapscofield <clarisse.scofield@gmail.com>
2022-05-19 10:46:37 -0300

    Criação base front e back

---
+++ backend/lectio/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=120)
+    author = models.CharField(max_length=120)
+    description = models.TextField()
+
+    def _str_(self):
+        return self.title
